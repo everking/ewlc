0f80dae27c74b5e96a91f2944c0bac1f20beaa0f
Eric de Guzman <eric_deguzman@bmc.com>
2023-01-30 15:07:15 -0800

    Initial update for EWLC: YT video.

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const embedId = "6fNmnlBC1LQ";
+  const allow = "accelerometer; "
+    + "autoplay; "
+    + "clipboard-write;" 
+    + "encrypted-media; "
+    + "gyroscope; " 
+    + "picture-in-picture";
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="video-container">
+        <iframe
+          className="video"
+          src={`https://www.youtube.com/embed/${embedId}`}
+          allow={allow}
+          allowFullScreen 
+          title="EWLC Video"
+        />
+      </div>
     </div>
   );
 }
