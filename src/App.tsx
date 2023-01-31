0707ced4fd04911b160ff1a0a8357d4f27e1ee39
Eric de Guzman <eric_deguzman@bmc.com>
2023-01-30 18:52:00 -0800

    Content changes

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,7 @@ function App() {
     + "encrypted-media; "
     + "gyroscope; " 
     + "picture-in-picture";
+  const homeImage = "https://www.eastwoodleadershipcamp.org/uploads/b/f99ed5cc0904b932b1989dfa8dc7e24ece9d4595bd2685b5b1d802b032bd5a58/IMG_20210714_172402950_HDR_1647317048.jpg?width=2000";
   return (
     <div className="App">
       <div className="video-container">
@@ -20,6 +21,7 @@ function App() {
           title="EWLC Video"
         />
       </div>
+      <img src={homeImage} />
     </div>
   );
 }
