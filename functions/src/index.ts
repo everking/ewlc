10e10d9b48b62734907e27027e6a925621e50526
Eric de Guzman <eric.deguzman@outlook.com>
2023-02-02 22:55:37 -0800

    fix lint issues.

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,7 +6,7 @@ admin.initializeApp();
 exports.addMessage = functions.https.onRequest(async (req, res) => {
   // Grab the text parameter.
   const original = req.query.text;
-  
+
   // Push the new message into Firestore using the Firebase Admin SDK.
   const writeResult = await admin.firestore()
       .collection("messages").add({original: original});
